48a0bb4c992de7a0f98508cf1feee6dbeef1801c
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-09-21 14:53:37 -0600

    initial commit

---
+++ src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-np9ey688.us.auth0.com'
+export const audience = 'https://dev-np9ey688.us.auth0.com/api/v2/'
+export const clientId = 'bOFiekvkK6WXz7nzmvt8sGMrInwwuWon'
---
+++ src/pages/CalendarPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Calendar</h1>
+  <input type="datetime-local" id="Test_DatetimeLocal" v-model="state.date" />
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+export default {
+  name: 'CalendarPage',
+  setup() {
+    const state = reactive({
+      date: ''
+    })
+    return { state }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
